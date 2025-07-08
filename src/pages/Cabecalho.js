f28b26d86f0ead468035c61fe3aca79079e50944
Vinicius Ielo Martins de Oliveira <162700647+viniciusielo4@users.noreply.github.com>
2025-07-08 09:25:01 -0300

    Add files via upload

---
+++ src/pages/Cabecalho.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Cabecalho() {
+  return (
+ <div></div>
+  )
+}
+
+export default Cabecalho
---
+++ src/pages/Cadastro.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Cadastro() {
+  return (
+    <div></div>
+  )
+}
+
+export default Cadastro
